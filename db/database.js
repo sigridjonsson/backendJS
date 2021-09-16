e01951a45244b71fb9c3e33e3665ce1d7c75535a
Sigrid Jönsson <sigridjonsson@Sigrids-MacBook-Pro.local>
2021-09-16 10:41:01 +0200

    Trouble with Travis again

--- db/database.js
+++ db/database.js
@@ -1,13 +1,27 @@
 const mongo = require("mongodb").MongoClient;
-const config = require("./config.json");
+// const config = require("./config.json");
 const collectionName = "crowd";
+
+let config;
+let username;
+let password;
+
+try {
+    config = require("./config.json");
+} catch (e) {
+    console.log(e);
+}
+
+username = process.env.USERNAME || config.username;
+password = process.env.PASSWORD || config.password;
+
 
 const database = {
     getDb: async function getDb () {
-        let dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.j9zvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+        let dsn = `mongodb+srv://${username}:${password}@cluster0.j9zvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
-            dsn = "mongodb://localhost:27017/test";
+            dsn = "mongodb://localhost:27017/test" ;
         }
 
         const client  = await mongo.connect(dsn, {
